66ccfdd2301d0201237027808c46ce03757b9684
bhumitgevariya-18 <bhumitgevariya45@gmail.com>
2025-02-21 11:39:05 +0530

    Added Jump Logic
    Created Jump method in PlayerMovementOld script with jumping logic

--- Assets/Scripts/PlayerMovementOld.cs
+++ Assets/Scripts/PlayerMovementOld.cs
@@ -7,6 +7,10 @@ public class PlayerMovementOld : MonoBehaviour
 {
     float speed;
     public Toggle runtoggle;
+    float t;
+    bool goup = false;
+    bool godown = false;
+    bool hasjumped = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,11 +28,50 @@ public class PlayerMovementOld : MonoBehaviour
         {
             speed = 5f;
         }
+        
         float xpos = Input.GetAxis("Horizontal");
         float ypos = Input.GetAxis("Vertical");
         
         Vector3 newpos = new Vector3(xpos,0,ypos);
 
         transform.position += newpos * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            hasjumped = true;
+            goup = true;
+        }
+        if (hasjumped)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        if (goup)
+        {
+            t += Time.deltaTime * 10;
+            float height = Mathf.Lerp(1.05f, 2.05f, t);
+            transform.position = new Vector3(transform.position.x, height, transform.position.z);
+            if (t >= 1)
+            {
+                t = 0;
+                goup = false;
+                godown = true;
+            }
+        }
+        else if (godown)
+        {
+            t += Time.deltaTime * 10;
+            float height = Mathf.Lerp(2.05f, 1.05f, t);
+            transform.position = new Vector3(transform.position.x, height, transform.position.z);
+            if (t >= 1)
+            {
+                t = 0;
+                godown = false;
+                hasjumped = false;
+            }
+        }
     }
 }
