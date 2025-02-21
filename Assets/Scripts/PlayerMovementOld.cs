5faef6d56b12ece510b7ce71dec40aee48d231ea
bhumitgevariya-18 <bhumitgevariya45@gmail.com>
2025-02-21 10:54:41 +0530

    Run and Walk Logic
    Added toggle for Run in Scene and adjusted speed in the PlayerMovementOld script accordingly

--- Assets/Scripts/PlayerMovementOld.cs
+++ Assets/Scripts/PlayerMovementOld.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovementOld : MonoBehaviour
 {
-    float speed = 10f;
+    float speed;
+    public Toggle runtoggle;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (runtoggle.isOn)
+        {
+            speed = 20f;
+        }
+        else
+        {
+            speed = 5f;
+        }
         float xpos = Input.GetAxis("Horizontal");
         float ypos = Input.GetAxis("Vertical");
         
