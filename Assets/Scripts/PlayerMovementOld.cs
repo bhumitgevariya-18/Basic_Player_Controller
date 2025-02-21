168d64a2e174ed8ebae8bc4a7dde53c76e753375
bhumitgevariya-18 <bhumitgevariya45@gmail.com>
2025-02-21 10:44:02 +0530

    Player Movement
    Added movement logic in the PlayerMovementOld script for player movement

--- Assets/Scripts/PlayerMovementOld.cs
+++ Assets/Scripts/PlayerMovementOld.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovementOld : MonoBehaviour
 {
+    float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,11 @@ public class PlayerMovementOld : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xpos = Input.GetAxis("Horizontal");
+        float ypos = Input.GetAxis("Vertical");
         
+        Vector3 newpos = new Vector3(xpos,0,ypos);
+
+        transform.position += newpos * speed * Time.deltaTime;
     }
 }
