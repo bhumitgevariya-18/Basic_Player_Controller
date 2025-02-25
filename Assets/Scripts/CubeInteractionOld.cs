4252fb9ea5c7ce6fc4951a67039058dc89197f36
bhumitgevariya-18 <bhumitgevariya45@gmail.com>
2025-02-25 11:03:31 +0530

    Touch Input in Task 2
    - Added rotation with single touch (Swipe) in Task 2
    - (Some settled anchors of joystick in Task 1 are included too)

--- Assets/Scripts/CubeInteractionOld.cs
+++ Assets/Scripts/CubeInteractionOld.cs
@@ -8,11 +8,9 @@ public class CubeInteractionOld : MonoBehaviour
     Vector3 mpos;
     float xpos;
     float ypos;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float zoomSpeed = 0.1f;
+    float minZoom = 2f;
+    float maxZoom = 15f;
 
     // Update is called once per frame
     void Update()
@@ -26,10 +24,6 @@ public class CubeInteractionOld : MonoBehaviour
             ypos = Input.GetAxis("Mouse Y");
         }
 
-        Vector3 newpos = new Vector3(ypos, -xpos, 0);
-
-        transform.Rotate(newpos * speed * Time.deltaTime,Space.World);
-
         if (Input.GetMouseButtonDown(1))
         {
             mpos = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
@@ -38,5 +32,77 @@ public class CubeInteractionOld : MonoBehaviour
         {
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition - mpos);
         }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (Input.touchCount == 1)
+            {
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    xpos = touch.deltaPosition.x;
+                    ypos = touch.deltaPosition.y;
+                }
+            }
+        }
+
+        Vector3 newpos = new Vector3(ypos, -xpos, 0);
+
+        transform.Rotate(newpos * speed * Time.deltaTime, Space.World);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//            // **Two Touches - Move & Zoom**
+//            else if (Input.touchCount == 2)
+//            {
+//                Touch touch1 = Input.GetTouch(0);
+//                Touch touch2 = Input.GetTouch(1);
+
+//                // **Move with two fingers**
+//                if (touch1.phase == TouchPhase.Moved && touch2.phase == TouchPhase.Moved)
+//                {
+//                    Vector2 avgDelta = (touch1.deltaPosition + touch2.deltaPosition) / 2;
+//                    Vector3 move = new Vector3(avgDelta.x, avgDelta.y, 0) * Time.deltaTime;
+//                    transform.position += move;
+//                }
+
+//                // **Zoom with pinch gesture (Limited)**
+//                float prevDistance = (touch1.position - touch1.deltaPosition - (touch2.position - touch2.deltaPosition)).magnitude;
+//                float currentDistance = (touch1.position - touch2.position).magnitude;
+//                float deltaDistance = currentDistance - prevDistance;
+
+//                if (Mathf.Abs(deltaDistance) > 0.1f)
+//                {
+//                    float newZoom = Camera.main.transform.position.z + deltaDistance * zoomSpeed;
+//                    newZoom = Mathf.Clamp(newZoom, -maxZoom, -minZoom); // Limit zoom
+//                    Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, newZoom);
+//                }
+//            }
+//        }
+//    }
+//}
+
